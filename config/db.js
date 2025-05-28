0e7bbf6029d07d560e28a709c1cde7622909ca51
pincode8485 <miftahfaridl.adam@gmail.com>
2025-05-29 03:17:55 +0700

    First

---
+++ config/db.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, Promise: null }
+}
+
+async function connectDb() {
+
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands:false
+        }
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/winstore8485`,opts).then( mongoose => {
+            return mongoose
+        })
+
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn
+    
+}
+
+export default connectDb
