35ea93a46e99bcc3c573d70d0c377db551ecc0df
pincode8485 <miftahfaridl.adam@gmail.com>
2025-05-30 23:49:17 +0700

    Second

---
+++ app/api/product/list/route.js
@@ -0,0 +1,16 @@
+import connectDb from "@/config/db"
+import Product from "@/models/Product"
+import { NextResponse } from "next/server"
+
+export async function GET(request) {
+    try {
+
+        await connectDb()
+
+        const products = await Product.find({})
+        return NextResponse.json({ success: true, products });
+
+    } catch (error) {
+        return NextResponse.json({ success: false, message: error.message });
+    }
+}
--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDb from "./db";
 import User from "@/models/user";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "winstore8485-next" });
@@ -55,5 +56,35 @@ export const syncUserDeletion = inngest.createFunction(
 
         await connectDb()
         await User.findByIdAndDelete(id)
+    }
+)
+
+// Inngest Function To Create User's Order In Database
+export const createUserOrder = inngest.createFunction(
+    {
+        id:'create-user-order',
+        batchEvents: {
+            maxSize: 25,
+            timeout: '5s'
+        }
+    },
+    {event: 'order/created'},
+    async ({events}) => {
+
+        const orders = events.map((event)=> {
+            return {
+                userId: event.data.userId,
+                items: event.data.items,
+                amount: event.data.amount,
+                address: event.data.address,
+                date: event.data.date
+            }
+        })
+
+        await connectDb()
+        await Order.insertMany(orders)
+
+        return { success: true, processed: orders.length };
+
     }
 )
